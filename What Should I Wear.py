2056e029a4a5ffff14c33fc4e2af931daef8c70a
Venomous-Python <157763520+Venomous-Python@users.noreply.github.com>
2024-01-28 16:51:28 -0700

    Add files via upload

--- What Should I Wear.py
+++ What Should I Wear.py
@@ -16,66 +16,62 @@ Grey = (200, 200, 200)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-
+#To make pygame have the ability to wait
 clock = pygame.time.Clock()
 
+#All of the text including the text size
 text_font = pygame.font.SysFont("Oswald", 50)
-
 text_font_Q = pygame.font.SysFont("Oswald", 40)
-
 text_font_D = pygame.font.SysFont("Oswald", 20)
 
+#Listing what values are needed to create text
 def draw_text(text, font, text_col, x, y):
     img = font.render(text, True, text_col)
     screen.blit(img, (x, y))
-
 def Question_or_Result(text, font, text_col, x, y):
     img = font.render(text, True, text_col)
     screen.blit(img, (x, y))
-
 def Small_Text(text, font, text_col, x, y):
     img = font.render(text, True, text_col)
     screen.blit(img, (x, y))
 
+#Allows us to display images using the word image
 pygame.display.set_caption('image')
 
 
-
+#All questions used to determing clothing
 Freezing = ("Is it Freezing?")
-
 Cold = ("Is it Cold?")
-
 Normal = ("Is it Normal?")
-
 Hot = ("Is it Hot?")
-
 Burning_Hot = ("Is it Burning Hot?")
-
 Rain = ("Is it Raining?")
-
 Mud = ("Is it Muddy?")
-
 Snowing = ("Is it Snowing?")
-
 A_Lot_Snow = ("Is there a lot of Snow?")
-
 Little_Snow = ("Is there a little amount of Snow?")
 
+#A hidden feature which will make the user quit if no is clicked to much
 Troll = ("Stop Trolling!")
 Troll_1 = ("I'm Serious!")
 Troll_2 = ("I dare you, do it again...")
 Troll_3 = False
 
+#These values will help us skip questions that won't be possible like asking if it is snowing when it is 20 degrees celuis
 Hot_Temp = False
 Normal_Temp = False
 Cold_Temp = False
 Freezing_Temp = False
 Raining = False
+Muddy = False
 
+#The first question is asking if it is freezing
 Questions = Freezing
 
+#Shows the degree under the question
 Degree = ("0 °C or below, 32 °F or below.")
 
+#This will give us our result
 Result_Accesory = False
 Result_Coat = False
 Result_Hoodie = False
@@ -83,53 +79,65 @@ Result_Shirt = False
 Result_Pants = False
 Result_Shoe = False
 
+#Types of shoes that can be shown
 Sandals = False
 Sneakers = False
 Winter_Boots = False
 Rain_Boots = False
 
+#Accesories or other things you may need
 Beanie = False
 Sunglasses = False
 
+#Different Coats
 Winter_Coat = False
 Light_Coat = False
 Rain_Coat = False
 No_Coat = False
 
+#A hoodie
 Hoodie = False
 
+#Types of pants
 Long_Pants = False
 Shorts = False
 
+#Types of shirts
 Long_Sleeve_Shirt = False
 T_Shirt = False
 
-Muddy = False
-
+#The result will be shown at the end
 Show_Result = False
 
+#This allows the colour to change easier in the text and background
 Colour_1 = White
 Colour_2 = White
 
+#The X and Y coordinate of the text so all questions can be shown
 x_coordinate = 200
 y_coordinate = 125
 
+#Y is yes and N is no
 Y = False
 N = False
 
+#r is pressed it is Restart
+Restart = False
+
+#When the program is running
 run = True
 while run:
 
+    #Screen will be White
     screen.fill((Colour_1))
 
+    #Text is displayed on the screen
     draw_text("What Should I Wear?", text_font, Colour_2, 125, 50)
-
     Question_or_Result(Questions, text_font_Q, Colour_2, x_coordinate, y_coordinate)
-
     Small_Text(Degree, text_font_D, Colour_2, 200, 175)
-
     Small_Text("Use Y for yes and N for no.", text_font_D, Black, 225, 350)
 
+    #When Y is pressed it is yes and when N is pressed it is no
     key = pygame.key.get_pressed()
     if key[pygame.K_y] == True:
         clock.tick(1)
@@ -137,8 +145,64 @@ while run:
     elif key[pygame.K_n] == True:
         clock.tick(1)
         N = True
+    if key[pygame.K_r] == True:
+        clock.tick(1)
+        Restart = True
+
+    #Restarts the program
+    if Restart == True:
+        Restart = False
+        Freezing = ("Is it Freezing?")
+        Cold = ("Is it Cold?")
+        Normal = ("Is it Normal?")
+        Hot = ("Is it Hot?")
+        Burning_Hot = ("Is it Burning Hot?")
+        Rain = ("Is it Raining?")
+        Mud = ("Is it Muddy?")
+        Snowing = ("Is it Snowing?")
+        A_Lot_Snow = ("Is there a lot of Snow?")
+        Little_Snow = ("Is there a little amount of Snow?")
+        Troll = ("Stop Trolling!")
+        Troll_1 = ("I'm Serious!")
+        Troll_2 = ("I dare you, do it again...")
+        Troll_3 = False
+        Hot_Temp = False
+        Normal_Temp = False
+        Cold_Temp = False
+        Freezing_Temp = False
+        Raining = False
+        Muddy = False
+        Questions = Freezing
+        Degree = ("0 °C or below, 32 °F or below.")
+        Result_Accesory = False
+        Result_Coat = False
+        Result_Hoodie = False
+        Result_Shirt = False
+        Result_Pants = False
+        Result_Shoe = False
+        Sandals = False
+        Sneakers = False
+        Winter_Boots = False
+        Rain_Boots = False
+        Beanie = False
+        Sunglasses = False
+        Winter_Coat = False
+        Light_Coat = False
+        Rain_Coat = False
+        No_Coat = False
+        Hoodie = False
+        Long_Pants = False
+        Shorts = False
+        Long_Sleeve_Shirt = False
+        T_Shirt = False
+        Show_Result = False
+        Colour_1 = White
+        Colour_2 = White
+        x_coordinate = 200
+        y_coordinate = 125
 
 
+    #Depending on your anwer it can take you to a different question and set some clothes to true and false
     if Questions == Freezing:
         x_coordinate = 200
         y_coordinate = 125
@@ -160,7 +224,6 @@ while run:
             clock.tick(1)
             Winter_Boots = True
             Questions = Snowing
-    
     if Questions == Cold:
         Colour_2 = White
         Colour_1 = ((0, 205, 255))
@@ -184,7 +247,6 @@ while run:
             clock.tick(1)
             Questions = Rain
             Y = False
-   
     if Questions == Normal:
         Colour_2 = ((255, 150, 0))
         Colour_1 = ((230, 230, 0))
@@ -206,7 +268,6 @@ while run:
             Rain_Boots = False
             clock.tick(1)
             Questions = Rain
-
     if Questions == Hot:
         Colour_2 = Red
         Colour_1 = ((255, 125, 0))
@@ -228,7 +289,6 @@ while run:
             Rain_Boots = False
             clock.tick(1)
             Questions = Rain
-    
     if Questions == Burning_Hot:
         Colour_2 = ((150, 0, 0))
         Colour_1 = Red
@@ -252,9 +312,7 @@ while run:
         if N == True:
             N = False
             clock.tick(1)
-            Questions = Troll
-
-    
+            Questions = Troll    
     if Questions == Troll:
         if N == True:
             N = False
@@ -264,7 +322,6 @@ while run:
             Y = False
             clock.tick(1)
             Questions = Freezing
-                
     if Questions == Troll_1:
         if N == True:
             N = False
@@ -274,7 +331,6 @@ while run:
             Y = False
             clock.tick(1)
             Questions = Freezing
-
     if Questions == Troll_2:
         x_coordinate = 100
         if N == True:
@@ -284,11 +340,10 @@ while run:
             Y = False
             clock.tick(1)
             Questions = Freezing
-
     if Troll_3 == True:
         run = False
-        
 
+        #This question may be skipped depending on your previous answer
     if Questions == Rain and Sandals == False:
         Colour_1 = ((0, 225, 255))
         Colour_2 = ((0, 130, 215))
@@ -309,6 +364,7 @@ while run:
             Rain_Boots = True
             Questions = Mud
 
+    #Depending on your previous answers and your current answer some questions may be skipped
     if Questions == Mud:
         Colour_2 = Dark_Brown
         Colour_1 = Brown
@@ -336,6 +392,7 @@ while run:
             if Cold_Temp == True:
                 Questions = A_Lot_Snow
 
+    #You can only get this question if you choose freezing
     if Questions == Snowing:
         Colour_1 = White
         Colour_2 = Grey
@@ -352,6 +409,7 @@ while run:
             clock.tick(1)
             Show_Result = True
             
+    #The amount of snow will change the result
     if Questions == A_Lot_Snow:
         Colour_2 = Grey
         Colour_1 = White
@@ -369,7 +427,6 @@ while run:
             Winter_Boots = True
             Rain_Boots = False
             Show_Result = True
-      
     if Questions == Little_Snow:
         Colour_1 = White
         Colour_2 = Grey
@@ -399,10 +456,11 @@ while run:
                 Rain_Boots = False
                 Show_Result = True
 
-
+    #This part of the code show the result depending on you answers and the images
     if Show_Result == True:
         screen.fill((0, 205, 255))
         draw_text("What Should I Wear?", text_font, Colour_2, 125, 50)
+        Small_Text("Press 'r' to restart.", text_font_D, Black, 225, 250)
         if Sandals == True:
             Colour_1 = Red
             Colour_2 = ((150, 0, 0))
@@ -450,7 +508,7 @@ while run:
         if No_Coat == True:
             Result_Coat = ("No coat")
             image = pygame.image.load('X.png')
-            
+
             screen.blit(image,(75, 275))
         if Long_Sleeve_Shirt == True:
             Result_Shirt = ("long sleeve shirt, ")
@@ -485,6 +543,7 @@ while run:
             image = pygame.image.load('X.png')
             screen.blit(image,(75, 75))
 
+        #Text that tells you your result
         Small_Text(Result_Shoe, text_font_D, Black, 375, 375)
         Small_Text(Result_Pants, text_font_D, Black, 375, 175)
         Small_Text(Result_Shirt, text_font_D, Black, 225, 375)
@@ -492,13 +551,13 @@ while run:
         Small_Text(Result_Hoodie, text_font_D, Black, 225, 175)
         Small_Text(Result_Accesory, text_font_D, Black, 75, 175)
 
-
+    #Allow you to close pygame
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-    
+    #Updates the screen
     pygame.display.update()
 
-
+#Closes pygame when run is false
 pygame.quit()
